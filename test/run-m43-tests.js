e63c72b6cdfdcfcc4b28726b6f17236abb02ff10
adityavarma1213-star <adityavarma1213@gmail.com>
2026-08-24 23:37:33 -0500

    test(m43): expand scholarship finder contract checks

--- test/run-m43-tests.js
+++ test/run-m43-tests.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const fs=require('fs'),assert=require('assert'),vm=require('vm');
-const c={window:{localStorage:{getItem:()=>null,setItem:()=>{},removeItem:()=>{},key:()=>null,length:0}}};
+const c={window:{localStorage:{getItem:()=>null,setItem:()=>{},removeItem:()=>{},key:()=>null,length:0}},URLSearchParams,fetch:async()=>({ok:true,json:async()=>({ok:true,results:[]})})};
 vm.createContext(c);
 vm.runInContext(fs.readFileSync('js/baa-scholarships.js','utf8'),c);
 const api=c.window.BAAScholarships;
 assert.ok(api);
-assert.deepEqual(c.window.BAAScholarships.filter(null,{}).results,[]);
+assert.deepEqual(api.filter(null,{}).results,[]);
+assert.equal(api.filter([{country:'IN',level:'9',fields:['science']},{country:'US'}],{country:'IN'}).results.length,1);
+assert.equal(api.rank([{matchScore:2},{matchScore:9}]).results[0].matchScore,9);
+assert.equal(typeof api.mount,'function');
 console.log('M43 PASS');
