09002b5a5baf8d0daaf053f70f68e1f4db186d11
adityavarma1213-star <adityavarma1213@gmail.com>
2026-08-27 18:22:54 -0500

    test(m48): cover fresh authenticated collaboration transport

--- test/run-m48-client-tests.js
+++ test/run-m48-client-tests.js
@@ -2,6 +2,6 @@
 const fs=require('fs'),assert=require('assert');
 const bridge=fs.readFileSync('js/baa-m48-collaboration-server.js','utf8');
 const catalogue=fs.readFileSync('js/baa-guide-catalogue.js','utf8');
-for(const token of ['BAAM48Server','credentials:\'include\'','createServerPost','commentServerPost','reportServerPost','moderateServerPost'])assert.ok(bridge.includes(token),`M48 bridge missing ${token}`);
+for(const token of ['BAAM48Server','credentials:\'include\'','cache:\'no-store\'','Accept:\'application/json\'','createServerPost','commentServerPost','reportServerPost','moderateServerPost'])assert.ok(bridge.includes(token),`M48 bridge missing ${token}`);
 assert.ok(catalogue.includes("js/baa-m48-collaboration-server.js"));
-console.log('M48 client bridge PASS (7/7 structural checks)');
+console.log('M48 client bridge PASS (9/9 structural checks)');
